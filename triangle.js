544764e69e529834d574ab3ad1dc16bdbca65e6c
JustCallMeJoJo <59923599+JustCallMeJoJo@users.noreply.github.com>
2020-02-12 10:14:37 +0100

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,3 +1,19 @@
 /**
 * Write a loop that makes seven calls to console.log to output the following triangle
 */
+
+let x = "#"
+for (jojo=0; jojo<7; jojo++)
+{console.log(x);
+  x += "#";}
+  
+  
+/*
+console.log("#")
+console.log("##")
+console.log("###")
+console.log("####")
+console.log("#####")
+console.log("######")
+console.log("#######")
+*/
