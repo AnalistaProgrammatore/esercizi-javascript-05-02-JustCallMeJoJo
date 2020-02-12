a0539e164dc7a12443248c1e4bb59cdd9394e469
JustCallMeJoJo <59923599+JustCallMeJoJo@users.noreply.github.com>
2020-02-12 10:16:36 +0100

    Update chessboard.js

--- chessboard.js
+++ chessboard.js
@@ -3,3 +3,36 @@
 * At each position of the grid there is either a space or a "#" character. 
 * The characters should form a chessboard.
 */
+
+
+/*
+for (let x=0; x<8; x++) {
+  if (x%2==0) {
+    console.log(" # # # #")}
+  else console.log("# # # # ")}
+*/
+
+
+let size = 8;
+let jojo = "";
+for (let x=0; x<size; x++) {
+  for (let y=0; y<size; y++) {
+    if ((x+y)%2==0) {
+      jojo += " "; }
+    else {
+      jojo += "#"; }
+  }
+  jojo += "\n"; }
+console.log(jojo);
+
+
+/*
+console.log(" # # # #");
+console.log("# # # # ");
+console.log(" # # # #");
+console.log("# # # # ");
+console.log(" # # # #");
+console.log("# # # # ");
+console.log(" # # # #");
+console.log("# # # # ");
+*/
